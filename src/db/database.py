a0229dd3e6fb11403d88fb07b10d9d32bae1fea3
luke-h1 <luke.howsam@yahoo.com>
2022-09-29 16:47:24 +0100

    add register route

--- src/db/database.py
+++ src/db/database.py
@@ -5,8 +5,13 @@ import random
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+
+    # exclude password from json response
+    serialize_rules = "-password"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
@@ -20,7 +25,7 @@ class User(db.Model):
 
 
 class Bookmark(db.Model):
-    __tablename__ = 'bookmarks'
+    __tablename__ = "bookmarks"
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text, nullable=True)
     url = db.Column(db.Text, nullable=False)
@@ -31,15 +36,15 @@ class Bookmark(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
     def generate_short_characters(self):
-            characters = string.digits+string.ascii_letters
-            picked_chars = ''.join(random.choices(characters, k=3))
+        characters = string.digits + string.ascii_letters
+        picked_chars = "".join(random.choices(characters, k=3))
 
-            link = self.query.filter_by(short_url=picked_chars).first()
+        link = self.query.filter_by(short_url=picked_chars).first()
 
-            if link:
-                self.generate_short_characters()
-            else:
-                return  picked_chars
+        if link:
+            self.generate_short_characters()
+        else:
+            return picked_chars
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -18,3 +18,49 @@ from flask_jwt_extended import (
 from flasgger import swag_from
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+
+
+@auth.post("/register")
+@swag_from("./docs/auth/register.yml")
+def register():
+    username = request.json["username"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    if len(password) < 6:
+        return (
+            jsonify({"error": "password must be more than 6 characters"}),
+            HTTP_400_BAD_REQUEST,
+        )
+
+    if len(username) < 3:
+        return (
+            jsonify({"error": "username must be more than 6 characters"}),
+            HTTP_400_BAD_REQUEST,
+        )
+
+    # would use custom serializers in bigger applications to parse this instead
+    # of writing directly in the blueprint
+    if not username.isalnum() or " " in username:
+        return jsonify(
+            {"error": "username should be alphanumeric & include no spaces"},
+            HTTP_400_BAD_REQUEST,
+        )
+
+    if not validators.email(email):
+        return jsonify({"error": "email must be a valid email"}), HTTP_400_BAD_REQUEST
+
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({"error": "Username or email already exists"}), HTTP_409_CONFLICT
+
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({"error": "Username or email already exists"}), HTTP_409_CONFLICT
+
+    pwd_hash = generate_password_hash(password)
+
+    user = User(username=username, password=pwd_hash, email=email)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created", "user": user}), HTTP_201_CREATED
